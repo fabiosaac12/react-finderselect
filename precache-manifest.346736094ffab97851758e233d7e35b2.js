6e287502b377649d323249a07703e2b40a309a9d
isaak <fabiosaac12@gmail.com>
2021-01-22 08:25:31 -0400

    Updates

---
+++ precache-manifest.346736094ffab97851758e233d7e35b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3ec30c66f382a1934b12ffbaa07ff4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "935d9b8b5cdbda96e464",
+    "url": "./static/css/main.8e396217.chunk.css"
+  },
+  {
+    "revision": "c96deafc6cd9c0c53e10",
+    "url": "./static/js/2.4a408ae3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4a408ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935d9b8b5cdbda96e464",
+    "url": "./static/js/main.0b165af8.chunk.js"
+  },
+  {
+    "revision": "d29d68ae65fa388c1e76",
+    "url": "./static/js/runtime-main.ddd98dd1.js"
+  },
+  {
+    "revision": "299139fcd448070c4361294868917324",
+    "url": "./static/media/chargingImage.299139fc.png"
+  }
+]);
